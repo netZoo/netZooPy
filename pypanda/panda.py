8e323f4951747bac4657c96c7b0e9dfdbe8db71e
Tian Wang <36548138+twangxxx@users.noreply.github.com>
2019-06-05 16:21:08 -0400

    Update panda.py
    the datatype of "force" column is character, so groupby() and sum() don't work properly.

--- pypanda/panda.py
+++ pypanda/panda.py
@@ -332,11 +332,13 @@ class Panda(object):
         #subset_indegree = self.export_panda_results.loc[:,['gene','force']]
         export_panda_results_pd = pd.DataFrame(self.export_panda_results,columns=['tf','gene','motif','force'])
         subset_indegree = export_panda_results_pd.loc[:,['gene','force']]
+        subset_indegree['force']=pd.to_numeric(subset_indegree.force)
         self.panda_indegree = subset_indegree.groupby('gene').sum()
         return self.panda_indegree
     def return_panda_outdegree(self):
         '''Return Panda outdegree.'''
         export_panda_results_pd = pd.DataFrame(self.export_panda_results,columns=['tf','gene','motif','force'])
         subset_outdegree = export_panda_results_pd.loc[:,['tf','force']]
+        subset_indegree['force']=pd.to_numeric(subset_indegree.force)
         self.panda_outdegree = subset_outdegree.groupby('tf').sum()
         return self.panda_outdegree
