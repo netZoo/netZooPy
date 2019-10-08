074d1cb588be886554bfa62b83d9f3cfa571a0ea
marouenbg <marouen.b.guebila@gmail.com>
2019-10-08 19:45:08 -0400

    less precise comparison

--- tests/test_panda.py
+++ tests/test_panda.py
@@ -19,5 +19,5 @@ def test_panda():
     res=pd.read_csv(gt_file, sep='\t', header=None)
     gt =pd.read_csv(output_file, sep='\t', header=None)
 
-    assert(res.equals(gt))
+    pandas.testing.assert_frame_equal(res,gt,check_less_precise=True)
     print('Test panda passed was successful!')
