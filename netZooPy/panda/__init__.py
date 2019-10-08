5ba6458a89338e940556fd918625bcaa31bacd96
marouenbg <marouen.b.guebila@gmail.com>
2019-10-08 19:17:54 -0400

    added packages

--- netZooPy/panda/__init__.py
+++ netZooPy/panda/__init__.py
@@ -1,6 +1,4 @@
 from __future__ import absolute_import
 
 from .panda import Panda
-#from .lioness import Lioness
 from .analyze_panda import AnalyzePanda
-#from .analyze_lioness import AnalyzeLioness
