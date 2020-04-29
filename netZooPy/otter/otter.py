68c0e7f52e21e06f8a60c20f8f639c6e8b8070d5
Rebekka Burkholz <rebekka.burkholz@gmail.com>
2020-04-29 15:39:30 -0400

    bug fixes

--- netZooPy/otter/otter.py
+++ netZooPy/otter/otter.py
@@ -11,18 +11,18 @@ def otter(W, P, C, lam = 0.0035, gamma = 0.335, Iter = 300, eta = 0.00001, bexp 
     b1t = b1**bexp
     b2t = b2**bexp
 
-    nTF, nGenes = W.shape
-    P = P * (-(1-lam)/np.trace(P)) + (1-lam) * 0.0013
+    t, g = W.shape
+    P = P * (-(1-lam)/np.trace(P)) - (1-lam) * 0.0013
     C = C * (-lam /np.trace(C))
     W = P@W
-    W = W/sqrt(np.trace(W @ W.T))
-    P = P + gamma*np.identity(nTF)
-    m = np.zeros((nTF, nGenes))
-    v = np.zeros((nTF, nGenes))
+    W = W/(-sqrt(np.trace(W @ W.T)))
+    P = P + gamma*np.identity(t)
+    m = np.zeros((t, g))
+    v = np.zeros((t, g))
 
     for i in range(Iter):
         grad = W@W.T@W + P@W + W@C
-        m = 1 * m + (4 * (1-b1)) * grad
+        m = b1 * m + (4 * (1-b1)) * grad
         v = b2 * v + (16 * (1-b2)) * grad**2
         b1t = b1t * b1
         b2t = b2t * b2
@@ -30,3 +30,5 @@ def otter(W, P, C, lam = 0.0035, gamma = 0.335, Iter = 300, eta = 0.00001, bexp 
         epst = eps * sqrt((1-b2t))
         W = W - alpha*(m/(epst + np.sqrt(v)))
     return W
+
+
