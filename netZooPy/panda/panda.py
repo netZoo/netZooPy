variable fix, calculation still off

netZooPy/panda/panda.py
@@ -210,11 +210,14 @@ class Panda(object):
         # Auxiliary dicts
         gene2idx = {x: i for i,x in enumerate(self.gene_names)}
         tf2idx = {x: i for i,x in enumerate(self.unique_tfs)}
-        if modeProcess=="union":
+        if modeProcess=="union" or modeProcess=="intersection":
             # Initialize data
             self.expression = np.zeros((self.num_genes, self.expression_data.shape[1]))
             # Populate gene expression
-            idx_geneEx = [gene2idx.get(x, 0) for x in self.expression_genes]
+            # if modeProcess=="union":
+            #     idx_geneEx = [gene2idx.get(x, 0) for x in self.expression_genes]
+            # else:
+            idx_geneEx = [gene2idx.get(x, 0) for x in self.gene_names]
             print(self.expression.shape)
             print(self.expression_data.shape)
             self.expression[idx_geneEx,:] = self.expression_data.values