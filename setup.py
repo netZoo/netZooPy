064a461fc514eeb319a053be42a38a2723ba228b
Marouen <marouenbg@users.noreply.github.com>
2020-08-24 15:39:20 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='netZooPy',
-    version='0.5',
+    version='0.7.2',
     description='Python implementation of netZoo.',
     url='https://github.com/netZoo/netZooPy',
     author='netZoo team',
