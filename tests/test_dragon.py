e4984e93e3f9958e9d67d2976b977355c0aba919
marouenbg <marouen.b.guebila@gmail.com>
2021-03-07 20:14:53 -0500

    added test for dragon

--- tests/test_dragon.py
+++ tests/test_dragon.py
@@ -12,13 +12,17 @@ def test_dragon():
                                             p1=100, p2=500, epsilon=[0.1,0.1],
                                             n=n, seed=123)
     lambdas, lambdas_landscape = dragon.estimate_penalty_parameters_dragon(X1, X2)
+    lambdas=tuple([int(1000*x)/1000 for x in lambdas]) # 3 digit precision
+    alamb=int(lambdas_landscape[1,1]*1000)/1000
     assert(lambdas == (0.907, 0.913))
-    assert(lambdas_landscape[1,1] == 398.778)
+    assert(alamb == 398.778)
 
     #2. test2
     r = dragon.get_partial_correlation_dragon(X1, X2, lambdas)
     adj_p_vals, p_vals = dragon.estimate_p_values_dragon(r, n, p1, p2, lambdas)
-    assert(p_vals[2,1] == 0.963)
-    assert(adj_p_vals[2,1] == 0.999)
+    adj_p_vals=int(adj_p_vals[2,1]*1000)/1000 # 3 digit precision
+    p_vals=int(p_vals[2,1]*1000)/1000
+    assert(p_vals == 0.963)
+    assert(adj_p_vals == 0.999)
 
      
