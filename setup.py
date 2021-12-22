b7e28479f3232e5a5084554e7290af04f8781901
marouenbg <marouen.b.guebila@gmail.com>
2021-12-22 11:32:00 -0500

    update condor

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='netZooPy',
-    version='0.8.1',
+    version='0.8.2',
     description='Python implementation of netZoo.',
     url='https://github.com/netZoo/netZooPy',
     author='netZoo team',
