3012a733a598ed1925f437ea699029bed4091612
violafanfani <viol.fanfani@gmail.com>
2022-02-08 10:31:39 -0500

    separated cpu and gpu

--- netZooPy/panda/calculations.py
+++ netZooPy/panda/calculations.py
@@ -58,46 +58,54 @@ def update_diagonal(diagonal_matrix, num, alpha, step):
     np.fill_diagonal(diagonal_matrix, diagonal_fill)
     return diagonal_matrix
 
+def compute_panda_cpu(
+    correlation_matrix,
+    ppi_matrix,
+    motif_matrix,
+    threshold=0.001,
+    alpha=0.1,
+):
+    """Panda network optimization
 
-def gt_function(x, y=None):
+    Args:
+        correlation_matrix (numpy float): coexpression matrix
+        ppi_matrix (numpy float): PPI network matrix
+        motif_matrix (numpy float): motif matrix
+        threshold (float, optional): hamming distance threshold for stop. Defaults to 0.001.
+        alpha (float, optional): learning rate. Defaults to 0.1
     """
-    Description:
-        Continuous Tanimoto similarity function computed on the GPU
-    Inputs:
-        x: First object to measure the distance from. If only this matrix is provided, then the distance is meausred between the columns of x.
-        y: Second object to measure the distance to
-    Ouputs:
-        a_matrix: Matrix containing the pairwsie distances.
-    """
-    if y is None:
-        a_matrix = cp.dot(x, x.T)
-        s = cp.square(x).sum(axis=1)
-        a_matrix /= cp.sqrt(s + s.reshape(-1, 1) - cp.abs(a_matrix))
-    else:
-        a_matrix = cp.dot(x, y)
-        a_matrix /= cp.sqrt(
-            cp.square(y).sum(axis=0)
-            + cp.square(x).sum(axis=1).reshape(-1, 1)
-            - cp.abs(a_matrix)
-        )
-    return a_matrix
+    motif_matrix = motif_matrix.copy()
+    ppi_matrix = ppi_matrix.copy()
+    correlation_matrix = correlation_matrix.copy()
 
+    num_tfs, num_genes = motif_matrix.shape
+    step = 0
+    hamming = 1
 
-def gupdate_diagonal(diagonal_matrix, num, alpha, step):
-    """
-    Description:
-        Updates the diagonal of the input matrix in the message passing computed on the GPU
-    Inputs:
-        diagonal_matrix: Input diagonal matrix.
-        num            : Number of rows/columns.
-        alpha          : Learning rate.
-        step           : The current step in the algorithm.
-    """
-    cp.fill_diagonal(diagonal_matrix, cp.nan)
-    diagonal_std = cp.nanstd(diagonal_matrix, 1)
-    diagonal_fill = diagonal_std * num * math.exp(2 * alpha * step)
-    cp.fill_diagonal(diagonal_matrix, diagonal_fill)
-    return diagonal_matrix
+    while hamming > threshold:
+        W = 0.5 * (
+            t_function(ppi_matrix, motif_matrix)
+            + t_function(motif_matrix, correlation_matrix)
+        )  # W = (R + A) / 2
+        hamming = np.abs(motif_matrix - W).mean()
+        # Update motif matrix
+        motif_matrix *= 1 - alpha
+        motif_matrix += alpha * W
+        # Update ppi_matrix
+        ppi = t_function(motif_matrix)  # t_func(X, X.T)
+        ppi = update_diagonal(ppi, num_tfs, alpha, step)
+        ppi_matrix *= 1 - alpha
+        ppi_matrix += alpha * ppi
+        # Update correlation_matrix
+        motif = t_function(motif_matrix.T)
+        motif = update_diagonal(motif, num_genes, alpha, step)
+        correlation_matrix *= 1 - alpha
+        correlation_matrix += alpha * motif
+        # del W, ppi, motif  # release memory for next step
+        print("step: {}, hamming: {}".format(step, hamming))
+        step = step + 1
+
+    return motif_matrix
 
 
 def compute_panda(
@@ -118,75 +126,25 @@ def compute_panda(
         threshold (float, optional): hamming distance threshold for stop. Defaults to 0.001.
         alpha (float, optional): learning rate. Defaults to 0.1
     """
-    motif_matrix = motif_matrix.copy()
-    ppi_matrix = ppi_matrix.copy()
-    correlation_matrix = correlation_matrix.copy()
-
-    num_tfs, num_genes = motif_matrix.shape
-    step = 0
-    hamming = 1
 
     if computing == "cpu":
         # Initialise W and hamming
-
-        while hamming > threshold:
-            W = 0.5 * (
-                t_function(ppi_matrix, motif_matrix)
-                + t_function(motif_matrix, correlation_matrix)
-            )  # W = (R + A) / 2
-            hamming = np.abs(motif_matrix - W).mean()
-            # Update motif matrix
-            motif_matrix *= 1 - alpha
-            motif_matrix += alpha * W
-            # Update ppi_matrix
-            ppi = t_function(motif_matrix)  # t_func(X, X.T)
-            ppi = update_diagonal(ppi, num_tfs, alpha, step)
-            ppi_matrix *= 1 - alpha
-            ppi_matrix += alpha * ppi
-            # Update correlation_matrix
-            motif = t_function(motif_matrix.T)
-            motif = update_diagonal(motif, num_genes, alpha, step)
-            correlation_matrix *= 1 - alpha
-            correlation_matrix += alpha * motif
-            # del W, ppi, motif  # release memory for next step
-            print("step: {}, hamming: {}".format(step, hamming))
-            step = step + 1
+        motif_matrix = compute_panda_cpu(
+            correlation_matrix,
+            ppi_matrix,
+            motif_matrix,
+            alpha=alpha,
+        )
 
     elif computing == "gpu":
-        import cupy as cp
+        from netZooPy.panda.calculations_gpu import compute_panda_gpu
 
-        ppi_matrix = cp.array(ppi_matrix)
-        motif_matrix = cp.array(motif_matrix)
-        correlation_matrix = cp.array(correlation_matrix)
-
-        if hamming > 0.001:
-
-            W = 0.5 * (
-                gt_function(ppi_matrix, motif_matrix)
-                + gt_function(motif_matrix, correlation_matrix)
-            )  # W = (R + A) / 2
-            hamming = cp.abs(motif_matrix - W).mean()
-            # update motif matrix
-            motif_matrix *= 1 - alpha
-            motif_matrix += alpha * W
-            # Update ppi_matrix
-            ppi = gt_function(motif_matrix)  # t_func(X, X.T)
-            ppi = gupdate_diagonal(ppi, num_tfs, alpha, step)
-            ppi_matrix *= 1 - alpha
-            ppi_matrix += alpha * ppi
-
-            # Update correlation_matrix
-            motif = gt_function(motif_matrix.T)
-            motif = gupdate_diagonal(motif, num_genes, alpha, step)
-            correlation_matrix *= 1 - alpha
-            correlation_matrix += alpha * motif
-            # del W, ppi, motif  # release memory for next step
-            print("step: {}, hamming: {}".format(step, hamming))
-            step = step + 1
-
-        motif_matrix = cp.asnumpy(motif_matrix)
-    else:
-        sys.error("ERROR: %s not recognised as computing choice" % computing)
+        motif_matrix = compute_panda_gpu(
+            correlation_matrix,
+            ppi_matrix,
+            motif_matrix,
+            alpha=alpha,
+        )
 
     return motif_matrix
 
--- netZooPy/puma/calculations.py
+++ netZooPy/puma/calculations.py
@@ -10,18 +10,16 @@ from .timer import Timer
 from netZooPy.panda.panda import Panda
 from netZooPy.panda.calculations import (
     t_function,
-    update_diagonal,
-    gt_function,
-    gupdate_diagonal,
+    update_diagonal
 )
 
 
-def compute_puma(
+
+def compute_puma_cpu(
     correlation_matrix,
     motif_matrix,
     ppi_matrix,
     sorted_index,
-    computing="cpu",
     alpha=0.1,
     threshold=0.001,
 ):
@@ -52,80 +50,89 @@ def compute_puma(
     # Alessandro
     TFCoopInit = ppi_matrix.copy()
 
-    if computing == "cpu":
+    ppi_matrix = ppi_matrix.copy()
+    motif_matrix = motif_matrix.copy()
+    correlation_matrix = correlation_matrix.copy()
 
-        ppi_matrix = ppi_matrix.copy()
-        motif_matrix = motif_matrix.copy()
-        correlation_matrix = correlation_matrix.copy()
-
-        while hamming > threshold:
-            W = 0.5 * (
+    while hamming > threshold:
+        W = 0.5 * (
                 t_function(ppi_matrix, motif_matrix)
                 + t_function(motif_matrix, correlation_matrix)
-            )  # W = (R + A) / 2
-            hamming = np.abs(motif_matrix - W).mean()
-            motif_matrix *= 1 - alpha
-            motif_matrix += alpha * W
+        )  # W = (R + A) / 2
+        hamming = np.abs(motif_matrix - W).mean()
+        motif_matrix *= 1 - alpha
+        motif_matrix += alpha * W
 
-            ppi = t_function(motif_matrix)  # t_func(X, X.T)
-            ppi = update_diagonal(ppi, num_tfs, alpha, step)
-            ppi_matrix *= 1 - alpha
-            ppi_matrix += alpha * ppi
+        ppi = t_function(motif_matrix)  # t_func(X, X.T)
+        ppi = update_diagonal(ppi, num_tfs, alpha, step)
+        ppi_matrix *= 1 - alpha
+        ppi_matrix += alpha * ppi
 
-            # Alessandro
-            TFCoopDiag = ppi_matrix.diagonal()
-            ppi_matrix[s1] = TFCoopInit[s1]
-            ppi_matrix[:, s1] = TFCoopInit[:, s1]
-            np.fill_diagonal(ppi_matrix, TFCoopDiag)
+        # Alessandro
+        TFCoopDiag = ppi_matrix.diagonal()
+        ppi_matrix[s1] = TFCoopInit[s1]
+        ppi_matrix[:, s1] = TFCoopInit[:, s1]
+        np.fill_diagonal(ppi_matrix, TFCoopDiag)
 
-            # Update correlation_matrix
-            motif = t_function(motif_matrix.T)  # t_func(X.T, X)
-            motif = update_diagonal(motif, num_genes, alpha, step)
-            correlation_matrix *= 1 - alpha
-            correlation_matrix += alpha * motif
+        # Update correlation_matrix
+        motif = t_function(motif_matrix.T)  # t_func(X.T, X)
+        motif = update_diagonal(motif, num_genes, alpha, step)
+        correlation_matrix *= 1 - alpha
+        correlation_matrix += alpha * motif
 
-            print("step: {}, hamming: {}".format(step, hamming))
-            step = step + 1
+        print("step: {}, hamming: {}".format(step, hamming))
+        step = step + 1
+
+    return motif_matrix
+
+def compute_puma(
+    correlation_matrix,
+    motif_matrix,
+    ppi_matrix,
+    sorted_index,
+    computing="cpu",
+    alpha=0.1,
+    threshold=0.001,
+):
+
+    """
+    Description:
+        The PUMA algorithm.
+
+    Inputs:
+        correlation_matrix: Input coexpression matrix.
+        motif_matrix      : Input motif regulation prior network.
+        ppi_matrix        : Input PPI matrix.
+        computing         : 'cpu' uses Central Processing Unit (CPU) to run PANDA.
+                            'gpu' use the Graphical Processing Unit (GPU) to run PANDA.
+
+    Uses (from panda):
+        t_function      : Continuous Tanimoto similarity function computed on the CPU.
+        update_diagonal : Updates the diagonal of the input matrix in the message passing computed on the CPU.
+        gt_function     : Continuous Tanimoto similarity function computed on the GPU.
+        gupdate_diagonal: Updates the diagonal of the input matrix in the message passing computed on the GPU.
+    """
+
+
+
+    if computing == "cpu":
+        motif_matrix = compute_puma_cpu(
+            correlation_matrix,
+            motif_matrix,
+            ppi_matrix,
+            sorted_index,
+            alpha=alpha,
+        )
 
     elif computing == "gpu":
-
-        import cupy as cp
-
-        ppi_matrix = cp.array(ppi_matrix)
-        motif_matrix = cp.array(motif_matrix)
-        correlation_matrix = cp.array(correlation_matrix)
-
-        while hamming > threshold:
-            W = 0.5 * (
-                gt_function(ppi_matrix, motif_matrix)
-                + gt_function(motif_matrix, correlation_matrix)
-            )  # W = (R + A) / 2
-            hamming = cp.abs(motif_matrix - W).mean()
-            motif_matrix = cp.array(motif_matrix)
-            motif_matrix *= 1 - alpha
-            motif_matrix += alpha * W
-
-            ppi = gt_function(motif_matrix)  # t_func(X, X.T)
-            ppi = gupdate_diagonal(ppi, num_tfs, alpha, step)
-            ppi_matrix *= 1 - alpha
-            ppi_matrix += alpha * ppi
-
-            # Alessandro
-            TFCoopDiag = ppi_matrix.diagonal()
-            ppi_matrix[s1] = TFCoopInit[s1]
-            ppi_matrix[:, s1] = TFCoopInit[:, s1]
-            np.fill_diagonal(ppi_matrix, TFCoopDiag)
-
-            # Update correlation_matrix
-            motif = gt_function(motif_matrix.T)
-            motif = gupdate_diagonal(motif, num_genes, alpha, step)
-            correlation_matrix *= 1 - alpha
-            correlation_matrix += alpha * motif
-
-            print("step: {}, hamming: {}".format(step, hamming))
-            step = step + 1
-        motif_matrix = cp.asnumpy(motif_matrix)
-
+        from netZooPy.puma.calculations_gpu import compute_puma_gpu
+        motif_matrix = compute_puma_gpu(
+            correlation_matrix,
+            motif_matrix,
+            ppi_matrix,
+            sorted_index,
+            alpha=alpha,
+        )
     else:
         sys.error("ERROR: %s is not an existing computing device")
 
