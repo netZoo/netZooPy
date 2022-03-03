146059cd7eb33c9ac9ea4cc1d517b4b3079549e9
violafanfani <viol.fanfani@gmail.com>
2022-03-03 18:02:30 -0500

    fixing rtd

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,7 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath("../../netZooPy"))
+sys.path.insert(0, os.path.abspath("../netZooPy"))
 sys.path.insert(0, os.path.abspath("../../netZooPy/docs/"))
 
 
