63ab001d41176e14869e60a409e960455b1aa40e
marouenbg <marouen.b.guebila@gmail.com>
2022-10-05 13:09:15 -0400

    use zscore with ddof=1

--- netZooPy/panda/calculations.py
+++ netZooPy/panda/calculations.py
@@ -167,7 +167,7 @@ def normalize_network(x):
     if x.shape[0] == x.shape[1]:
         norm_row = norm_col.T
     else:
-        norm_row = zscore(x, axis=1)
+        norm_row = zscore(x, ddof=1, axis=1)
     # Alessandro: replace nan values
     normalized_matrix = (norm_col + norm_row) / math.sqrt(2)
     norm_total = (x - np.mean(x)) / np.std(x)  # NB zscore(x) is not the same
