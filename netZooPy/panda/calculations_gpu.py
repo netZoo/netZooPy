3f2feac1c3ea6970b7d43f261a3dab6726412f75
violafanfani <viol.fanfani@gmail.com>
2022-10-23 22:14:27 -0400

    trying to bring everything to single

--- netZooPy/panda/calculations_gpu.py
+++ netZooPy/panda/calculations_gpu.py
@@ -22,25 +22,16 @@ def gt_function(x, y=None):
     Ouputs:
         a_matrix: Matrix containing the pairwsie distances.
     """
-    print('remove gt')
-    print(x.dtype)
     if y is None:
         a_matrix = cp.dot(x, x.T)
         s = cp.square(x).sum(axis=1)
-        print('noy')
-        print(s.dtype)
-        print(a_matrix.dtype)
         a_matrix /= cp.sqrt(s + s.reshape(-1, 1) - cp.abs(a_matrix))
     else:
         a_matrix = cp.dot(x, y)
-        print('y')
-        print(a_matrix.dtype)
-        print(x.dtype)
-        print(y.dtype)
         a_matrix /= cp.sqrt(
-            cp.square(y).sum(axis=0)
-            + cp.square(x).sum(axis=1).reshape(-1, 1)
-            - cp.abs(a_matrix)
+            cp.square(y).sum(axis=0).astype(float32)
+            + cp.square(x).sum(axis=1).reshape(-1, 1).astype(float32)
+            - cp.abs(a_matrix).astype(float32)
         )
     return a_matrix
 
