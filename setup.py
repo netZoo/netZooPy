f5539487c78bb7873671c18db3300dbb82aedeb0
violafanfani <viol.fanfani@gmail.com>
2023-10-13 13:34:07 -0400

    Bump version: 0.9.16 → 0.9.17

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='netZooPy',
-    version='0.9.16',
+    version='0.9.17',
     description='Python implementation of netZoo.',
     url='https://github.com/netZoo/netZooPy',
     author='netZoo team',
