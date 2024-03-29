5ebf1de92f4596cea1860703ca6888ab5f98e95a
Brian Lawney <blawney@hsph.harvard.edu>
2024-03-29 12:10:02 -0400

    PANDA- adding guard against invalid gene IDs and test case. (#343)
    * PANDA- adding guard against invalid gene IDs and test case.
    
    * Added missing comparison. Tests pass
    
    * Remove unnecessary import introduced by IDE
    
    ---------
    
    Co-authored-by: Brian Lawney <brianlawney@hsph.harvard.edu>

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ copyright = u'2019, netZoo'
 author    = u'netZoo'
 
 # The short X.Y version
-__version__ = "0.10.1"
+__version__ = "0.10.2"
 version     = ".".join(__version__.split(".")[:2])
 # The full version, including alpha/beta/rc tags
 release     = __version__
